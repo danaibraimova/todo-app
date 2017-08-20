47f8e928b56b74f52611d534d78e3b51a7fcb1b6
Dana Ibraimova <Dana Ibraimova>
2017-08-21 00:01:34 +0600

    adding and deleting todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {TodosComponent} from './todos/todos.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //directives: [TodosComponent]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos;
+  text;
+  constructor() { }
+
+  ngOnInit() {
+    this.todos=[
+      {
+        text: 'sdfdsf'
+
+      },
+      {
+        text: 'sdfs'
+      }
+    ];
+  }
+
+  addTodo()
+  {
+    this.todos.push({
+      text: this.text
+    })
+  }
+
+  deleteTodo(todoText)
+  {
+    for(var i=0;i<this.todos.length;i++)
+      {
+        if(this.todos[i].text==todoText)
+          {
+            this.todos.splice(i,1);
+          }
+      }
+  }
+
+}
